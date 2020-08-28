25466c759b41bf179c82a6fff9b7fd735a910def
jayden.tan <jayden.tan@vrviu.com>
2020-08-28 16:18:49 +0800

    build static library

--- build.sh
+++ build.sh
@@ -8,4 +8,9 @@ export webrtc_library_debug="/home/jayden/share/opensource/webrtc_m72/src/out/De
 export webrtc_library_release="/home/jayden/share/opensource/webrtc_m72/src/out/Release/obj/libwebrtc.a"
 export ffmpeg_root_dir="/home/jayden/toolchains/vcpkg/installed/x64-linux-shared/"
 
-cmake .. -DWITH_WEBRTC=ON  -DWEBRTC_ROOT_DIR=$webrtc_root_dir -DWEBRTC_INCLUDE_DIR=$webrtc_root_dir -DWEBRTC_LIBRARY_DEBUG=$webrtc_library_debug -DWEBRTC_LIBRARY_RELEASE=$webrtc_library_release -DWITH_FFMPEG=ON -DFFMPEG_ROOT_DIR=$ffmpeg_root_dir -DCMAKE_CXX_STANDARD=17 -DBUILD_SAMPLES=OFF -DBUILD_TESTS=OFF
+cmake .. -DWITH_WEBRTC=ON -DBUILD_SHARED_LIBS=OFF  -DWEBRTC_ROOT_DIR=$webrtc_root_dir \
+-DWEBRTC_INCLUDE_DIR=$webrtc_root_dir \
+-DWEBRTC_LIBRARY_DEBUG=$webrtc_library_debug \
+-DWEBRTC_LIBRARY_RELEASE=$webrtc_library_release -DWITH_FFMPEG=ON \
+-DFFMPEG_ROOT_DIR=$ffmpeg_root_dir -DCMAKE_CXX_STANDARD=17 -DBUILD_SAMPLES=OFF \
+-DBUILD_TESTS=OFF -DCMAKE_INSTALL_PREFIX=./stage
